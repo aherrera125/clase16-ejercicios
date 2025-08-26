7fd2e4d63365e6e3acb9592beb66ed8a283cd458
aherrera125 <j.alejandroherrera@gmail.com>
2025-08-26 19:35:10 -0300

    Cambiar de lugar algunas funciones

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -10,18 +10,18 @@ formDataE6.addEventListener("submit", function (e) {
     }
   }
 
-  function esPrimo(numero) {
-    if (numero < 2) return false; // 0 y 1 no son primos
-
-    for (let i = 2; i <= Math.sqrt(numero); i++) {
-      if (numero % i === 0) {
-        return false; // si tiene un divisor, no es primo
-      }
-    }
-    return true; // si no encontramos divisores, es primo
-  }
-
   formDataE6.querySelector(
     "#showDisplayE6"
   ).textContent = `Los números primos de 1 a 100 son: ${primoArray}`;
 });
+
+function esPrimo(numero) {
+  if (numero < 2) return false; // 0 y 1 no son primos
+
+  for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) {
+      return false; // si tiene un divisor, no es primo
+    }
+  }
+  return true; // si no encontramos divisores, es primo
+}
--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -13,24 +13,6 @@ formDataE9.addEventListener("submit", (e) => {
   let masJoven = personaMasJoven(personas);
   let promedioTotal = promedioEdades(personas);
 
-  function personaMasJoven(personas) {
-    let edad = 100;
-    for (let i = 0; i < personas.length; i++) {
-      if (personas[i].edad < edad) {
-        edad = personas[i].edad;
-      }
-    }
-    return personas.filter((p) => p.edad == edad);
-  }
-
-  function promedioEdades(personas) {
-    let edadPromedio = 0;
-    for (let i = 0; i < personas.length; i++) {
-      edadPromedio += parseInt(personas[i].edad);
-    }
-    return edadPromedio / parseInt(personas.length);
-  }
-
   formDataE9.querySelector("#showDisplayE9").textContent = "VER LA CONSOLA.";
 
   console.log(
@@ -39,3 +21,21 @@ formDataE9.addEventListener("submit", (e) => {
 
   console.log(`La edad promedio es ${promedioTotal}`);
 });
+
+function personaMasJoven(personas) {
+  let edad = 100;
+  for (let i = 0; i < personas.length; i++) {
+    if (personas[i].edad < edad) {
+      edad = personas[i].edad;
+    }
+  }
+  return personas.filter((p) => p.edad == edad);
+}
+
+function promedioEdades(personas) {
+  let edadPromedio = 0;
+  for (let i = 0; i < personas.length; i++) {
+    edadPromedio += parseInt(personas[i].edad);
+  }
+  return edadPromedio / parseInt(personas.length);
+}
